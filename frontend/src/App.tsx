f5045f61f4350763873a3ee28f8905ce21084bbc
lim ho <dlagh123@gmail.com>
2023-11-02 22:23:21 +0900

    gitAction Test 2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>임호 홈페이지다 우와~</h1>
+      <h1>임호 홈페이지다 우와~ㅋㅋㅋㅋㅋ</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
